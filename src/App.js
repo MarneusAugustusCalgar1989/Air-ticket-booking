eec3367a9ecf9a7af81f0628b104efdbdeae4add
unknown <egor@westsib.ru>
2024-02-06 01:13:41 +0700

    Make some styles

--- src/App.js
+++ src/App.js
@@ -1,61 +1,61 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import FlyCard from './components/FlyCard';
-import FilterByTransfer from './components/FilterByTransfer';
-import SortBy from './components/SortBy';
-import InputData from './components/InputData';
-import WeeklyFlyCard from './components/WeeklyFlyCard';
+import { useEffect, useState } from 'react'
+import './App.css'
+import FlyCard from './components/FlyCard'
+import FilterByTransfer from './components/FilterByTransfer'
+import SortBy from './components/SortBy'
+import InputData from './components/InputData'
+import WeeklyFlyCard from './components/WeeklyFlyCard'
 
 function App() {
-  const [airlineData, setAirlineData] = useState([]);
-  const [myData, setMyData] = useState([]);
-  const [ready, setReady] = useState(false);
-  const [dataToSend, setDataToSend] = useState({ def: 'def' });
+  const [airlineData, setAirlineData] = useState([])
+  const [myData, setMyData] = useState([])
+  const [ready, setReady] = useState(false)
+  const [dataToSend, setDataToSend] = useState({ def: 'def' })
 
-  const [sorters, setSorters] = useState('price');
-  const [listingLoader, setListingLoader] = useState(true);
+  const [sorters, setSorters] = useState('price')
+  const [listingLoader, setListingLoader] = useState(true)
 
-  const [noTickets, setNoTickets] = useState(false);
+  const [noTickets, setNoTickets] = useState(false)
 
-  const [spinner, setSpinner] = useState(false);
-  const [smallSpinner, setSmallSpinner] = useState(false);
+  const [spinner, setSpinner] = useState(false)
+  const [smallSpinner, setSmallSpinner] = useState(false)
 
-  const [filters, setFilters] = useState(['4']);
+  const [filters, setFilters] = useState(['4'])
 
   useEffect(() => {
-    airlineQuery();
-  }, [window]);
+    airlineQuery()
+  }, [window])
 
-  const getFilters = value => {
-    setFilters(value);
-  };
+  const getFilters = (value) => {
+    setFilters(value)
+  }
 
-  const takeData = value => {
-    setDataToSend(value);
+  const takeData = (value) => {
+    setDataToSend(value)
 
-    setReady(true);
+    setReady(true)
 
     if (Object.keys(dataToSend).length > 2) {
-      setSmallSpinner(true);
+      setSmallSpinner(true)
     } else {
-      setSpinner(true);
+      setSpinner(true)
     }
-  };
+  }
 
   const airlineQuery = () => {
     fetch('http://213.59.156.172:3000/get_airlines')
-      .then(response => response.json())
-      .then(data => {
-        setAirlineData(data);
-        setReady(true);
-      });
-  };
+      .then((response) => response.json())
+      .then((data) => {
+        setAirlineData(data)
+        setReady(true)
+      })
+  }
 
   useEffect(() => {
-    ticketQuery();
-  }, [dataToSend]);
+    ticketQuery()
+  }, [dataToSend])
 
-  const initData = `{"query":"{prices_one_way(params: {origin: \\"${dataToSend.originCode}\\", destination: \\"${dataToSend.destinationCode}\\", depart_months: \\"${dataToSend.flightDate}\\", no_lowcost: false}paging: {limit: 10, offset:0}sorting: ROUTE_WEIGHT_DESC currency:\\"RUB\\" grouping: DEPART_DATE)  {departure_at value currency distance duration ticket_link number_of_changes main_airline trip_class}}"}`;
+  const initData = `{"query":"{prices_one_way(params: {origin: \\"${dataToSend.originCode}\\", destination: \\"${dataToSend.destinationCode}\\", depart_months: \\"${dataToSend.flightDate}\\", no_lowcost: false}paging: {limit: 10, offset:0}sorting: ROUTE_WEIGHT_DESC currency:\\"RUB\\" grouping: DEPART_DATE)  {departure_at value currency distance duration ticket_link number_of_changes main_airline trip_class}}"}`
 
   const ticketQuery = () => {
     fetch('http://213.59.156.172:3000/ql', {
@@ -63,14 +63,14 @@ function App() {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(initData),
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         if (data.data) {
-          setMyData(data.data.prices_one_way);
-          setNoTickets(false);
-          setListingLoader(false);
-          setSpinner(false);
-          setSmallSpinner(false);
+          setMyData(data.data.prices_one_way)
+          setNoTickets(false)
+          setListingLoader(false)
+          setSpinner(false)
+          setSmallSpinner(false)
           // myData.map(el => {
           //   if (
           //     el.departure_at.split('T')[0].toString() === dataToSend.flightDate
@@ -79,38 +79,41 @@ function App() {
           //   }
           // });
           if (data.data.prices_one_way.length === 0) {
-            console.log('Таких билетов нету');
-            setNoTickets(true);
+            console.log('Таких билетов нету')
+            setNoTickets(true)
           }
         }
-      });
-  };
+      })
+  }
 
   return (
-    <div className='App'>
+    <div className="App">
       {!ready && (
-        <div className='app_loader'>
+        <div className="app_loader">
           <h1>Loading...</h1>
         </div>
       )}
       {ready && (
-        <div className='app_wrapper'>
-          <h1>ЛОГО</h1>
+        <div className="app_wrapper">
+          {/* <h1>ЛОГО</h1> */}
+          <div className="image_holder">
+            <img className="main_logo" src="./images/logo.png"></img>
+          </div>
           {airlineData.length > 0 && (
-            <div className='inputField'>
+            <div className="inputField">
               <InputData takeData={takeData} />
             </div>
           )}
 
           {spinner && (
-            <div className='app_loader small'>
+            <div className="app_loader small">
               <h1>Loading...</h1>
             </div>
           )}
 
           {!listingLoader && (
             <SortBy
-              className='sortBy'
+              className="sortBy"
               myData={dataToSend}
               flightData={myData}
               setSorters={setSorters}
@@ -119,7 +122,7 @@ function App() {
           )}
           {!listingLoader && (
             <FilterByTransfer
-              className='filterByTransfer'
+              className="filterByTransfer"
               myData={dataToSend}
               flightData={myData}
               setFilters={setFilters}
@@ -128,19 +131,19 @@ function App() {
             />
           )}
 
-          <div className='flycarHolder'>
+          <div className="flycardHolder">
             {smallSpinner && (
-              <div className='app_loader small'>
+              <div className="app_loader small">
                 <h1>Loading...</h1>
               </div>
             )}
             {noTickets && (
-              <div className='app_loader small'>
+              <div className="app_loader small">
                 <h1>Таких билетов нету</h1>
               </div>
             )}
             {!smallSpinner &&
-              myData.map(el => {
+              myData.map((el) => {
                 return (
                   <WeeklyFlyCard
                     flyData={el}
@@ -148,16 +151,16 @@ function App() {
                     airlineData={airlineData}
                     key={el.departure_at}
                   />
-                );
+                )
               })}
           </div>
         </div>
       )}
     </div>
-  );
+  )
 }
 //  token
 // {
 //   "X-Access-token": "bc99cead7d4f7cbf0b85723bcb406ea0"
 //   }
-export default App;
+export default App
--- src/components/FilterByTransfer.jsx
+++ src/components/FilterByTransfer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 const FilterByTransfer = ({
   myData,
@@ -7,121 +7,121 @@ const FilterByTransfer = ({
   getFilters,
   flightData,
 }) => {
-  const checkListener = e => {
-    const flyCards = document.querySelectorAll('.transfers');
+  const checkListener = (e) => {
+    const flyCards = document.querySelectorAll('.transfers')
 
-    const explr = filters;
+    const explr = filters
     if (explr.length > 0 && explr.includes(e.target.value)) {
-      explr.splice(explr.indexOf(e.target.value), 1);
+      explr.splice(explr.indexOf(e.target.value), 1)
     } else {
-      explr.push(e.target.value);
+      explr.push(e.target.value)
     }
     if (filters.length === 0) {
-      explr.push('4');
-      document.getElementById('noSort').checked = true;
+      explr.push('4')
+      document.getElementById('noSort').checked = true
     }
     if (filters.length > 1 && filters.includes('4')) {
-      document.getElementById('noSort').checked = false;
-      explr.splice(explr.indexOf('4'), 1);
+      document.getElementById('noSort').checked = false
+      explr.splice(explr.indexOf('4'), 1)
     }
     if (e.target.value === '4') {
-      document.querySelectorAll('.checkbox').forEach(el => {
-        el.childNodes[0].checked = false;
-        explr.splice(0, explr.length, '4');
+      document.querySelectorAll('.checkbox').forEach((el) => {
+        el.childNodes[0].checked = false
+        explr.splice(0, explr.length, '4')
 
-        document.getElementById('noSort').checked = true;
-      });
+        document.getElementById('noSort').checked = true
+      })
     }
 
-    setFilters(explr);
+    setFilters(explr)
 
     if (explr[0] === '4') {
-      flyCards.forEach(el => {
-        el.parentNode.style.visibility = 'visible';
-        el.parentNode.style.display = 'inherit';
-      });
+      flyCards.forEach((el) => {
+        el.parentNode.style.visibility = 'visible'
+        el.parentNode.style.display = 'inherit'
+      })
     } else {
-      flyCards.forEach(el => {
+      flyCards.forEach((el) => {
         if (!explr.includes(el.textContent.slice(-1))) {
-          el.parentNode.style.visibility = 'collapse';
-          el.parentNode.style.display = 'none';
+          el.parentNode.style.visibility = 'collapse'
+          el.parentNode.style.display = 'none'
         } else {
-          el.parentNode.style.visibility = 'visible';
-          el.parentNode.style.display = 'inherit';
+          el.parentNode.style.visibility = 'visible'
+          el.parentNode.style.display = 'inherit'
         }
-      });
+      })
     }
-  };
+  }
   return (
-    <div>
-      <div className='filterButtons'>
+    <div className="filterButtons_wrapper">
+      <div className="filterButtons">
         <legend>
           <h4>Количество пересадок</h4>
         </legend>
-        <div className='checkbox'>
+        <div className="checkbox">
           <input
-            type='checkbox'
-            id='noSort'
-            name='transferSort'
-            value='4'
-            onChange={e => {
-              checkListener(e);
+            type="checkbox"
+            id="noSort"
+            name="transferSort"
+            value="4"
+            onChange={(e) => {
+              checkListener(e)
             }}
             defaultChecked
           />
-          <label htmlFor='noSort'>Все билеты</label>
+          <label htmlFor="noSort">Все билеты</label>
         </div>
-        <div className='checkbox'>
+        <div className="checkbox">
           <input
-            type='checkbox'
-            id='noTransfers'
-            name='transferSort'
-            value='0'
-            onChange={e => {
-              checkListener(e);
+            type="checkbox"
+            id="noTransfers"
+            name="transferSort"
+            value="0"
+            onChange={(e) => {
+              checkListener(e)
             }}
           />
-          <label htmlFor='noTransfer'>Без пересадок</label>
+          <label htmlFor="noTransfer">Без пересадок</label>
         </div>
-        <div className='checkbox'>
+        <div className="checkbox">
           <input
-            type='checkbox'
-            id='oneTransfer'
-            name='transferSort'
-            value='1'
-            onChange={e => {
-              checkListener(e);
+            type="checkbox"
+            id="oneTransfer"
+            name="transferSort"
+            value="1"
+            onChange={(e) => {
+              checkListener(e)
             }}
           />
-          <label htmlFor='OneTransfer'>1</label>
+          <label htmlFor="OneTransfer">Одна</label>
         </div>
-        <div className='checkbox'>
+        <div className="checkbox">
           <input
-            type='checkbox'
-            id='twoTransfers'
-            name='transferSort'
-            value='2'
-            onChange={e => {
-              checkListener(e);
+            type="checkbox"
+            id="twoTransfers"
+            name="transferSort"
+            value="2"
+            onChange={(e) => {
+              checkListener(e)
             }}
           />
-          <label htmlFor='twoTransfers'>2</label>
+          <label htmlFor="twoTransfers">Две</label>
         </div>
-        <div className='checkbox'>
+        <div className="checkbox">
           <input
-            type='checkbox'
-            id='threeTransfers'
-            name='transferSort'
-            value='3'
-            onChange={e => {
-              checkListener(e);
+            type="checkbox"
+            id="threeTransfers"
+            name="transferSort"
+            value="3"
+            onChange={(e) => {
+              checkListener(e)
             }}
           />
-          <label htmlFor='twoTransfers'>3</label>
+          <label htmlFor="twoTransfers">Три</label>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default FilterByTransfer;
+export default FilterByTransfer
