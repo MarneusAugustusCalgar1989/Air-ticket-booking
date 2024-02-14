5363b8fe12b6a52c35757f50119c8ef95bdc096a
MarneusAugustusCalgar1989 <egor@westsib.ru>
2024-02-14 23:06:34 +0700

    Add main icon instead of picture

--- src/App.js
+++ src/App.js
@@ -1,73 +1,73 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import FlyCard from './components/FlyCard'
-import FilterByTransfer from './components/FilterByTransfer'
-import SortBy from './components/SortBy'
-import InputData from './components/InputData'
-import WeeklyFlyCard from './components/WeeklyFlyCard'
+import { useEffect, useState } from 'react';
+import './App.css';
+import FlyCard from './components/FlyCard';
+import FilterByTransfer from './components/FilterByTransfer';
+import SortBy from './components/SortBy';
+import InputData from './components/InputData';
+import WeeklyFlyCard from './components/WeeklyFlyCard';
 
 function App() {
-  const [airlineData, setAirlineData] = useState([])
-  const [myData, setMyData] = useState([])
-  const [ready, setReady] = useState(false)
-  const [dataToSend, setDataToSend] = useState({ def: 'def' })
+  const [airlineData, setAirlineData] = useState([]);
+  const [myData, setMyData] = useState([]);
+  const [ready, setReady] = useState(false);
+  const [dataToSend, setDataToSend] = useState({ def: 'def' });
 
-  const [sorters, setSorters] = useState('')
-  const [listingLoader, setListingLoader] = useState(true)
+  const [sorters, setSorters] = useState('');
+  const [listingLoader, setListingLoader] = useState(true);
 
-  const [noTickets, setNoTickets] = useState(false)
+  const [noTickets, setNoTickets] = useState(false);
 
-  const [spinner, setSpinner] = useState(false)
-  const [smallSpinner, setSmallSpinner] = useState(false)
+  const [spinner, setSpinner] = useState(false);
+  const [smallSpinner, setSmallSpinner] = useState(false);
 
-  const [filters, setFilters] = useState(['4'])
+  const [filters, setFilters] = useState(['4']);
 
-  const [clicked, setClicked] = useState(false)
-  const [ticketsLoaded, settTicketsLoaded] = useState(false)
+  const [clicked, setClicked] = useState(false);
+  const [ticketsLoaded, settTicketsLoaded] = useState(false);
 
-  const [error, setError] = useState('')
+  const [error, setError] = useState('');
 
-  const getClicked = (value) => {
-    setClicked(value)
-  }
+  const getClicked = value => {
+    setClicked(value);
+  };
 
   useEffect(() => {
-    airlineQuery()
-  }, [])
+    airlineQuery();
+  }, []);
 
-  const getFilters = (value) => {
-    setFilters(value)
-  }
+  const getFilters = value => {
+    setFilters(value);
+  };
 
-  const takeData = (value) => {
-    setDataToSend(value)
-    setReady(true)
+  const takeData = value => {
+    setDataToSend(value);
+    setReady(true);
 
     if (Object.keys(dataToSend).length > 2) {
-      setSmallSpinner(true)
+      setSmallSpinner(true);
     } else {
-      setSpinner(true)
+      setSpinner(true);
     }
-    setNoTickets(false)
-  }
+    setNoTickets(false);
+  };
 
   const airlineQuery = () => {
     fetch('http://213.59.156.172:3000/get_airlines')
-      .then((response) => response.json())
-      .then((data) => {
-        setAirlineData(data)
+      .then(response => response.json())
+      .then(data => {
+        setAirlineData(data);
       })
-      .catch((error) => setError(error.message))
+      .catch(error => setError(error.message))
       .finally(() => {
-        setReady(true)
-      })
-  }
+        setReady(true);
+      });
+  };
 
   useEffect(() => {
-    ticketQuery()
-  }, [dataToSend])
+    ticketQuery();
+  }, [dataToSend]);
 
-  const initData = `{"query":"{prices_one_way(params: {origin: \\"${dataToSend.originCode}\\", destination: \\"${dataToSend.destinationCode}\\", depart_months: \\"${dataToSend.flightDate}\\", no_lowcost: false}paging: {limit: 10, offset:0}sorting: ROUTE_WEIGHT_DESC currency:\\"RUB\\" grouping: DEPART_DATE)  {departure_at value currency distance duration ticket_link number_of_changes main_airline trip_class}}"}`
+  const initData = `{"query":"{prices_one_way(params: {origin: \\"${dataToSend.originCode}\\", destination: \\"${dataToSend.destinationCode}\\", depart_months: \\"${dataToSend.flightDate}\\", no_lowcost: false}paging: {limit: 10, offset:0}sorting: ROUTE_WEIGHT_DESC currency:\\"RUB\\" grouping: DEPART_DATE)  {departure_at value currency distance duration ticket_link number_of_changes main_airline trip_class}}"}`;
 
   const ticketQuery = () => {
     fetch('http://213.59.156.172:3000/ql', {
@@ -75,56 +75,57 @@ function App() {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(initData),
     })
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         if (data.data) {
-          setMyData(data.data.prices_one_way)
-          setNoTickets(false)
-          setListingLoader(false)
-          setSpinner(false)
-          setSmallSpinner(false)
+          setMyData(data.data.prices_one_way);
+          setNoTickets(false);
+          setListingLoader(false);
+          setSpinner(false);
+          setSmallSpinner(false);
 
           if (data.data.prices_one_way.length === 0) {
-            setNoTickets(true)
+            setNoTickets(true);
           }
         }
       })
-      .catch((error) => setError(error.message))
-      .finally(() => {})
-  }
+      .catch(error => setError(error.message))
+      .finally(() => {});
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       {!ready && (
-        <div className="app_loader">
-          <svg className="app_svg_spinner" viewBox="0 0 50 50">
-            <circle className="path" cx="25" cy="25" r="20"></circle>
+        <div className='app_loader'>
+          <svg className='app_svg_spinner' viewBox='0 0 50 50'>
+            <circle className='path' cx='25' cy='25' r='20'></circle>
           </svg>
         </div>
       )}
       {ready && (
-        <div className="app_wrapper">
+        <div className='app_wrapper'>
           {/* <h1>ЛОГО</h1> */}
-          <div className="image_holder">
-            <img className="main_logo" src="./images/logo.png"></img>
+          <div className='image_holder'>
+            {/* <img className='main_logo' src='./images/logo.png'></img> */}
+            <i class='bx bxs-plane-alt'></i>
           </div>
           {airlineData.length > 0 && (
-            <div className="inputField">
+            <div className='inputField'>
               <InputData takeData={takeData} />
             </div>
           )}
 
           {spinner && (
-            <div className="app_loader small">
-              <svg className="app_svg_spinner" viewBox="0 0 50 50">
-                <circle className="path" cx="25" cy="25" r="20"></circle>
+            <div className='app_loader small'>
+              <svg className='app_svg_spinner' viewBox='0 0 50 50'>
+                <circle className='path' cx='25' cy='25' r='20'></circle>
               </svg>
             </div>
           )}
 
           {!listingLoader && (
             <SortBy
-              className="sortBy"
+              className='sortBy'
               myData={dataToSend}
               flightData={myData}
               setSorters={setSorters}
@@ -135,7 +136,7 @@ function App() {
           )}
           {!listingLoader && (
             <FilterByTransfer
-              className="filterByTransfer"
+              className='filterByTransfer'
               myData={dataToSend}
               flightData={myData}
               setFilters={setFilters}
@@ -144,21 +145,21 @@ function App() {
             />
           )}
 
-          <div className="flycardHolder">
+          <div className='flycardHolder'>
             {smallSpinner && (
-              <div className="app_loader small">
-                <svg className="app_svg_spinner" viewBox="0 0 50 50">
-                  <circle className="path" cx="25" cy="25" r="20"></circle>
+              <div className='app_loader small'>
+                <svg className='app_svg_spinner' viewBox='0 0 50 50'>
+                  <circle className='path' cx='25' cy='25' r='20'></circle>
                 </svg>
               </div>
             )}
             {noTickets && (
-              <div className="app_loader small">
+              <div className='app_loader small'>
                 <h1>Таких билетов нету</h1>
               </div>
             )}
             {!smallSpinner &&
-              myData.map((el) => {
+              myData.map(el => {
                 return (
                   <WeeklyFlyCard
                     flyData={el}
@@ -168,16 +169,16 @@ function App() {
                     getClicked={getClicked}
                     clicked={clicked}
                   />
-                )
+                );
               })}
           </div>
         </div>
       )}
     </div>
-  )
+  );
 }
 //  token
 // {
 //   "X-Access-token": "bc99cead7d4f7cbf0b85723bcb406ea0"
 //   }
-export default App
+export default App;
